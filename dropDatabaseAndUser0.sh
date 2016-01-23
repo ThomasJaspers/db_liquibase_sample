0914381c5c0512c7b97f239cd444da64f545161c
Thomas Jaspers <tritom42@googlemail.com>
2016-01-23 16:35:08 +0100

    Added another Changeset

--- dropDatabaseAndUser0.sh
+++ dropDatabaseAndUser0.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS flywaydemo"
-psql -U postgres -d postgres -c "DROP ROLE IF EXISTS flywaydemo"
+psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS liquibasedemo"
+psql -U postgres -d postgres -c "DROP ROLE IF EXISTS liquibasedemo"
--- src/main/resources/liquibase/release_1_0_0/001_create_test_table.sql
+++ src/main/resources/liquibase/release_1_0_0/001_create_test_table.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset liquibase-demo:release_1_0_0.01_create_test_table.sql
+--changeset liquibase-demo:release_1_0_0.001_create_test_table.sql
 
 CREATE TABLE liquibase_test (
   key VARCHAR(64),
